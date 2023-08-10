5ebb3c831acda849cf20e5a858300f0091a79816
Giovani <giovani.o@outlook.com.br>
2023-08-10 10:59:10 -0300

    feat: Tipagem do tema

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+import "styled-components";
+import { defaultTheme } from "../styles/themes/default";
+
+// Arquivo de definição de tipos, guarda os tipos de defaultTheme em ThemeType
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+  // DefaultTheme do styled-components se torna uma extensão de theme type
+  export interface DefaultTheme extends ThemeType {} 
+}
