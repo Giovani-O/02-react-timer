f24b42080870b254ca870263fbf7284918468e24
Giovani <giovani.o@outlook.com.br>
2023-08-11 11:59:41 -0300

    feat: finaliza estilização

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,10 +11,25 @@ export function Home() {
             type="text" 
             id="task" 
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
 
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Mergear branches" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput 
+            type="number" 
+            id="minutesAmount" 
+            placeholder="00" 
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
