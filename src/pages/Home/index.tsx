9d878a6e83e2b5443f3581a1048218b9cbdea5cf
Giovani <giovani.o@outlook.com.br>
2023-08-14 11:53:30 -0300

    feat: iniciando ciclos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
-import { HomeContainer, FormContainer, CountdownContainer, Separator, StartCountdownButton, TaskInput, MinutesAmountInput } from "./styles";
-import { Play } from "phosphor-react";
+import { useState } from "react";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod'; // Resolver para integrar hook-form e zod
 import * as zod from 'zod' // Biblioteca de validações
+import { HomeContainer, FormContainer, CountdownContainer, Separator, StartCountdownButton, TaskInput, MinutesAmountInput } from "./styles";
+import { Play } from "phosphor-react";
 
 // Controlled and uncontrolled components
 // Controlled component sempre armazena os valores inseridos em um estado
@@ -19,7 +20,17 @@ const newCycleFormValidationSchema = zod.object({
 // Cria a "interface" através do zod.infer
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
   // Desconstruindo o retorno de useForm
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema), // Aplicando a validação
@@ -30,9 +41,32 @@ export function Home() {
   }); 
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id: id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+    
+    setCycles(state => [...state, newCycle]);
+    setActiveCycleId(id)
+
     reset();
   }
+
+  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  // padStart insere caracteres no início de uma string até ela alcançar o comprimento determinado
+  const minutes = String(minutesAmount).padStart(2, '0');
+  const seconds = String(secondsAmount).padStart(2, '0');
 
   const task = watch ('task');
   const isSubmitDisabled = !task;
@@ -72,11 +106,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
